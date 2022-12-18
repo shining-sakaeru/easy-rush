656518c1ce24c4af1e2b60cd901e358c19cc18fd
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-18 22:13:27 +0900

    easy-rust for today 221218

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -1,9 +1,29 @@
+use std::mem::size_of;
+
 fn main() {
     // one line comment
     /* inside comment */
     println!("Hello, world!");
 
+    //integer
     let a :u8 = 100;
     let b = 50;
     let c = a + b;
+
+    //char and string
+    let my_number: u16 = 8;
+    let second_number: u8 = 10;
+    let third_number = my_number + second_number as u16; // simple casting
+
+    println!("Size of a char: {} bytes", size_of::<char>());
+    println!("Size of a string : {}", "a".len());
+    println!("Size of a string : {}", "abcde".len());
+
+    let slice = "Hello";
+    println!("Slice is {} bytes, {} characters.", slice.len(), slice.chars().count());
+    let slice2 = "안녕!";
+    println!("Slice is {} bytes, {} characters.", slice2.len(), slice2.chars().count());
+
+
 }
+    
