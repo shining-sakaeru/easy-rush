efbf23dd2c57442069f79b4febac11e8ee0287d1
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-22 17:47:12 +0900

    easy-rust float, println
    q
    qui
    quit
    
    exit
    
    git
    
    q:1
    q!
    :q!

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     //char and string
     let my_number: u16 = 8;
     let second_number: u8 = 10;
-    let third_number = my_number + second_number as u16; // simple casting
+    //let third_number = my_number + second_number as u16; // simple casting
 
     println!("Size of a char: {} bytes", size_of::<char>());
     println!("Size of a string : {}", "a".len());
@@ -23,6 +23,24 @@ fn main() {
     println!("Slice is {} bytes, {} characters.", slice.len(), slice.chars().count());
     let slice2 = "안녕!";
     println!("Slice is {} bytes, {} characters.", slice2.len(), slice2.chars().count());
+
+    //float
+    //let float_number = 9_u8;
+    let my_num_f = 9.6;
+    let other_num_f = 9;
+    println!("{}", my_num_f + other_num_f as f64);
+
+    //println!
+    fn give_age() -> i32 {
+        36
+    }
+
+    let my_name = "Noah";
+    //let my_age = 36;
+    println!("My name is {} and my age is {}", my_name, give_age());
+
+
+
 
 
 }
