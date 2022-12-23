8ba92d33680467609c3c9b28ce0bd9e88b4a7e57
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-23 13:14:09 +0900

    functions

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -61,5 +61,17 @@ fn main() {
     println!("{:?}",tuple);
 
 
+    // functions
+    fn give_number(one: i32, two: i32) -> i32 {
+        let multiplied_by_ten =  {
+            let first_number = 10;
+            first_number * one * two
+        };
+        multiplied_by_ten
+    }
+    
+    let my_number = give_number(9, 1);
+    println!("{}", my_number);
+
 }
     
