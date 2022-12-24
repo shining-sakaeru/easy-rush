69b148f1b65c4b26c240e7573d4de568ee08cdec
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-24 23:01:09 +0900

    easy-rust Structs

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -428,12 +428,44 @@ exist"
     match_number(5);
     match_number(11);
 
+    // structs
+
+    // unit struct
+    struct FileDirectory;  // byte를 사용하지 않음
+
+    // tuple struct
+    #[derive(Debug)]  // attribute
+    struct Colour(u8, u8, u8);
+
+    // named struct
+    #[derive(Debug)]  // attribute
+    struct Country {
+        population: u32,
+        capital: String,
+        leader_name: String
+    }
 
 
+    fn takes_file_directory(_input: FileDirectory) {
+        println!("I got a file directory");
+    }
 
+    let x = FileDirectory;
+    println!("The size is {}", std::mem::size_of_val(&x));
 
+    let my_colour = Colour(20, 50, 100);
+    println!("The second colour is {}", my_colour.1);
+    println!("The second colour is {:?}", my_colour);
 
+    let canada = Country {
+        population: 35_000_000,
+        capital: "Ottawa".to_string(),
+        leader_name: "Justin Trudearu".to_string()
+    };
 
+    println!("The population is: {}\nThe capital is: {}", canada.population, canada.capital);
+
+    println!("The country is: {:#?}", canada);
 
 
 }
