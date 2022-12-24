1a603e5a6d0734985a2643b333fff9f303c6b72f
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-24 12:19:03 +0900

    easy-rust copy and clone

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -208,9 +208,26 @@ exist"
     let my_country2 = "Korean".to_string();
     add_is_great2(my_country2);
 
+    // It's trivial to copy the bytes
+    // Ownership and copy types
+    fn prints_number(number: i32) {
+        println!("{}", number);
+    }
 
+    fn prints_string(input: String) {
+        println!("{}", input);
+    }
 
+    // copy - copy types
+    // clone - non-copy types
 
+    let my_number = 8;
+    prints_number(my_number);
+    prints_number(my_number);
+
+    let my_country = "Korea".to_string();
+    prints_string(my_country.clone());
+    prints_string(my_country);
 
 }
     
