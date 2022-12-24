219718370c5335e55238f36ddbf165fe94497d3e
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-24 22:38:03 +0900

    easy-rust More match statements

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -389,6 +389,49 @@ exist"
         _ => println!("Something other")
     }
 
+    // rgb
+
+    fn match_colours(rbg: (u32, u32, u32)) {
+        match rbg {
+            (r, _, _) if r < 10 => println!("Not much red"),
+            (_, b, _) if b < 10 => println!("Not much blue"),
+            (_, _, g) if g < 10 => println!("Not much green"),
+           _ => println!("Every colour is at least 10")
+        }
+    }
+
+    let first = (200, 0, 0);
+    let second = (50, 50, 50);
+    let third = (200, 50, 0);
+
+    match_colours(first);
+    match_colours(second);
+    match_colours(third);
+    
+
+
+    let my_number = 10;
+    let some_variable = match my_number {
+        10 => 8,
+        _ => 000
+    };
+    println!("{}", some_variable);
+
+
+    fn match_number(input: i32) {
+        match input {
+            number @ 0..=10 => println!("It's between 0 and 10. It's the number {}", number),
+            _ => println!("It's greater than ten")
+        }
+    }
+
+    match_number(5);
+    match_number(11);
+
+
+
+
+
 
 
 
