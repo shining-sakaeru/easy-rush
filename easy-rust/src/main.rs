55d8e5bd4d56be62e48cacf113b13f00bd92554e
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-26 16:44:57 +0900

    easy-rust More impl blocks

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -691,6 +691,15 @@ exist"
         Dog
     }
 
+    impl Animal {  // impl 여러개 생성 가능
+        fn new_old_cat() -> Self {
+            Self {
+                age: 15,
+                animal_type: AnimalType::Cat
+            }
+        }
+    }
+
     impl Animal {
         fn new_cat(age: u8) -> Self {  // Self = Animal
             
@@ -699,16 +708,39 @@ exist"
                 animal_type: AnimalType::Cat
             }
         }
+        
+        fn new_dog(age: u8) -> Self {  // Self = Animal
+            
+            Self {
+                age,
+                animal_type: AnimalType::Dog
+            }
+        }
+
+        fn print(&self) {
+            println!("I am a: {:?}", self);
+        }
+
+        fn change_to_dog(&mut self) {
+            self.animal_type = AnimalType::Dog;
+            println!("Changed to dog! Now I am: {:?}", self);
+        }
+
+        fn change_to_cat(&mut self) {
+            self.animal_type = AnimalType::Cat;
+            println!("Changed to cat! Now I am: {:?}", self);
+        }
+
     }
 
-    let my_animal = Animal::new_cat(10); 
-
-    println!("I made a: {:?}", my_animal);
-
-
-
-
-
+    let mut my_animal = Animal::new_dog(10); 
+    //println!("I made a: {:?}", my_animal);
+    my_animal.print();  // syntactic sugar
+    //Animal::print(&my_animal);  // 위와 동일
+    my_animal.change_to_cat();
+    my_animal.change_to_dog();
+    
+    let my_old_cat = Animal::new_old_cat();
 
 
 
