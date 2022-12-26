2459d42bda48a49753088fd7d31b34cc6871e695
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-26 15:31:48 +0900

    easy-rust enums 3

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::{mem::size_of};
 
 fn main() {
@@ -586,9 +587,50 @@ exist"
         println!("The number is: {}", season as u32);
     }
 
+    // enums 3
+    enum Star {
+        BrownDwarf = 10,
+        RedDwarf = 50,
+        YellowStar = 100,
+        RedGiant = 1000,
+        DeadStar
+    }
+
+    use Star::*;
+    let starvec = vec![BrownDwarf, RedDwarf, YellowStar, RedDwarf, DeadStar];
+
+    for star in starvec {
+        match star as u32 {
+            size if size <= 80 => println!("Not the biggest star: {}", size),
+            size if size >= 80 => println!("Pretty big star: {}", size),
+            _ => println!("Some other star")
+        }
+    }
+
+    println!("What about DeadStar? It is: {}", DeadStar as u32);
 
 
+    enum Number {
+        U32(u32),
+        I32(i32)
+    }
 
+    fn get_number(input: i32) -> Number {
+        match input.is_positive() {
+            true => Number::U32(input as u32),
+            false => Number::I32(input),
+        }
+    }
+
+    let my_vec = vec![get_number(-800), get_number(8)];  // Vec<Number>
+
+    for item in my_vec {
+        match item {
+            Number::U32(number) => println!("It's a u32 value: {}", number),
+            Number::I32(number) => println!("It's a i32 value: {}", number),
+            
+        }
+    }
 
 
 
