0e2a68e47f30f1ada57c4c0d0bec16ea56a051f8
Noah Sim <nathanael@Noahs-MacBook-Air.local>
2022-12-31 14:36:12 +0900

    easy-rust Dereferencing and the dot operator

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -825,10 +825,28 @@ exist"
 
     println!("Person2 type is: {:?}", person2);
 
+    // Dereferencing and the dot operator
+    struct Item {
+        number: u8
+    }
+    
+    // . dot operator
+    impl Item {
+        fn compare_number(&self, other_number: u8) {
+            println!("Are they equal? {}", self.number == other_number)
+        }
+    }
 
+    let item = Item {
+        number: 10
+    };
 
+    let reference_item = &item;
+    let other_reference_item = &reference_item;  // &&item
 
-
+    item.compare_number(10);  // Rust는 자동 Deref 해줌
+    reference_item.compare_number(10);
+    other_reference_item.compare_number(10);
 
 }
     
